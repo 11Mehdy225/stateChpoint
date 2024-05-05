d5f2511d7b1cc0bec96d62aeb07772d192eea9d6
11Mehdy225 <loicmehdy11@outlook.fr>
2024-05-05 16:00:31 +0000

    monch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+
+
+class App extends React.Component 
+{
+
+state = {
+  divcontainer:false,
+}
+
+render ()
+{
+   const personne ={
+    fullname :'Konan Ndassenan Loic Harold Mehdy',
+    bio :'Diplômé en informatique et passionné par linnovation, je suis déterminé à mouvrir au monde professionnel et mettre mes compétences à profit. Ma passion pour le web et mon savoir-faire en matière dassistance informatique sont mes principaux atouts.',
+    profession :'informaticien developpeur dapplication',
+    contact : '0700000000 / 0100000000',
+    imgSrc:"/images/loic.JPG", 
+   };
+
+
+      var Handleclick = e =>
+      {
+        this.setState({divcontainer:!this.state.divcontainer});
+      }
+
+        const x =this.state.divcontainer;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>je vous presente l'avenir de ce pays dans le domaine du dev</h1>
+        <hr></hr>
+        <button onClick={Handleclick}>
+            {x?'hide':'show'}
+        </button>
+        {
+        x &&(<div>
+
+          <div className='gle_card'>
+            <section className='photo_pers'>
+              <img  src={personne.imgSrc}  alt='individu' className='image'/>
+            </section>
+            <section className='infos'>
+                <article className='sous_info'> <p>{personne.fullname}</p></article>
+                <article className='sous_info'><p>{personne.profession}</p></article>
+                <article className='sous_info'><p>{personne.contact}</p></article>
+            </section>
+            <section className='bio'>
+                  <p>
+                  {personne.bio}
+                  <hr></hr>
+                  {personne.bio}
+                  </p>
+            </section>
+          </div>
+          </div>)
+        }
+        </center>
+
     </div>
   );
+}
+
+
 }
 
 export default App;
